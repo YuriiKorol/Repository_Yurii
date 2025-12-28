c8c0c151110aeec9adff6e262322161aeb2077a0
Yurii <korolik1995nice@gmail.com>
2025-12-28 16:31:53 +0100

    Add customer spending analysis SQL script
    This SQL script calculates the average spending of customers in each category and identifies customers whose spending exceeds the category average, displaying the percentage difference.

---
+++ sql/customer_spending_analysis.sql
@@ -0,0 +1,28 @@
+/* Task: Calculate the average spending of all customers in each category.
+         For each customer, show by what percentage their spending is above or below the category average.
+         Display only the records where the customer spent more than the category average. */
+
+WITH cte AS (
+SELECT people.name AS p_name
+      ,categories.name AS cat_name
+      ,SUM(price)  AS tot_pers_in_cat
+FROM orders
+JOIN shop ON orders.shopId = shop.id
+JOIN people ON orders.personId = people.id           
+JOIN categories ON categories.id = shop.category_id
+GROUP BY people.id, categories.id
+ORDER BY people.id)
+,
+ranked AS (
+SELECT cat_name
+	  ,avg(tot_pers_in_cat) AS avg_in_category
+FROM cte
+GROUP BY cat_name)
+SELECT cte.p_name
+      ,cte.cat_name
+      ,cte.tot_pers_in_cat
+      ,ranked.avg_in_category
+	    ,round(((tot_pers_in_cat - avg_in_category) / avg_in_category) * 100, 2) AS diff_procent
+FROM cte
+JOIN ranked ON ranked.cat_name = cte.cat_name  
+WHERE cte.tot_pers_in_cat > ranked.avg_in_category;
